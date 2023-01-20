297b23bdc4cf07ef8ce2ae8bf7967fa92efd5135
anshbhatnagar <anshigrb@hotmail.co.uk>
2023-01-20 15:10:12 +0000

    edited etab1BE1Fsf and etab1BE1Fnatsf to implement the model of two sectors at the same temperature. etab1BE1Fsf is in terms of log(a) whereas etab1BE1Fnatsf is calculated in terms of a

--- ulysses/etab1BE1Fsf.py
+++ ulysses/etab1BE1Fsf.py
@@ -4,6 +4,7 @@ from scipy import interpolate
 from scipy.integrate import odeint
 from scipy.special import zeta
 from ulysses.numba import jit
+import matplotlib.pyplot as plt
 
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++#
@@ -11,16 +12,17 @@ from ulysses.numba import jit
 #+++++++++++++++++++++++++++++++++++++++++++++++++#
 
 @jit
-def fast_RHS(y0, a, rRADi, log10_ain, d, w1, epstt, epsmm, epsee, rnuRda_eq, Del, GCF):
-    rnuR         =      y0[0] # RHN
-    Tp           =      y0[1] # Temperature
-    NBL          =      y0[2] # B-L asymmetry
-    H            =      np.sqrt(8 * np.pi * GCF * rRADi * 10.**(4.*(log10_ain - a))/3.) #Hubble parameter
-    expression1  =      np.log(10.) * (rnuR -  rnuRda_eq) * d/H
-    dTda         =    - (Tp/Del) * np.log(10.)
+def fast_RHS(y0, a, Tp, gst, rRADi, ain, d, w1, epstt, epsmm, epsee, rnuRda_eq, Del, GCF):
+    rnuR      = y0[0] # RHN
+    NBL       = y0[1] # B-L asymmetry
+    
+    Mpl = np.sqrt(1/(8 * np.pi * GCF))
+    rho = np.pi**2/30.*(gst)*Tp**4
+    H            =      np.sqrt(rho/3.)/Mpl #Hubble parameter
+    expression1  =      (rnuR -  rnuRda_eq) * d/(a*H)
     drnuRda      =    - expression1
-    dNBLa        =      (epstt + epsmm + epsee) * expression1 - np.log(10.) * (w1/H) * NBL
-    return [drnuRda, dTda, dNBLa]
+    dNBLa        =      (epstt + epsmm + epsee) * expression1 -  (w1/(a*H)) * NBL
+    return [drnuRda, dNBLa]
 
 class EtaB_1BE1Fsf(ulysses.ULSBase):
     """
@@ -65,15 +67,16 @@ class EtaB_1BE1Fsf(ulysses.ULSBase):
 
     def flavourlabels(self): return ["$T$", "$NBL$"]
 
-    def RHS(self, y0, a, epstt, epsmm, epsee, rRADi, log10_ain):
-        Tp            = y0[1]
+    def RHS(self, y0, a, Tp, epstt, epsmm, epsee, rRADi, ain):
+        Tp = self.Ti/(a)
         z             = self.M1/Tp
         from ulysses.ulsbase import my_kn2, my_kn1
-        _d            = np.real(self.Gamma1* my_kn1(z) / my_kn2(z))
-        _w1           = _d * 0.25 * kn2 * z**2
+        kn2 = my_kn2(z)
+        _d       = np.real(self.Gamma1* my_kn1(z) / kn2)
+        _w1      = _d * 0.25 * kn2 * z**2
         rnuRda_eq     = self.N1Eq(z)
         Del           =  1. + Tp * self.ipol_dgstarSdT(Tp)/(3. * self.ipol_gstar(Tp)) # Temperature parameter
-        return fast_RHS(y0, a, rRADi, log10_ain, _d, _w1,  epstt, epsmm, epsee, rnuRda_eq, Del, self.GCF)
+        return fast_RHS(y0, a, Tp, self.ipol_gstar(Tp), rRADi, ain, _d, _w1,  epstt, epsmm, epsee, rnuRda_eq, Del, self.GCF)
 
     @property
     def EtaB(self):
@@ -81,29 +84,41 @@ class EtaB_1BE1Fsf(ulysses.ULSBase):
         epstt = np.real(self.epsilon1ab(2,2))
         epsmm = np.real(self.epsilon1ab(1,1))
         epsee = np.real(self.epsilon1ab(0,0))
-        Ti      = 100 * self.M1 # initial temp 100 greater than mass N1
-        rRadi   = np.pi**2 * self.ipol_gstar(Ti) / 30. * Ti**4 # initial radiation domination rho_RAD = pi^2* gstar(T[0])/30*T^4
-        y0      = np.array([0., Ti, 0.])
-        nphi    = (2.*zeta(3)/np.pi**2) * Ti**3
-        params  = np.array([epstt, epsmm, epsee, np.real(rRadi), 0.])
-        aflog10 = 5.0
-        t1 = np.linspace(0., aflog10, num=1000, endpoint=True)
+        self.Ti      = 100 * self.M1 # initial temp 100 greater than mass N1
+        Tp = self.Ti
+        rRadi   = np.pi**2 * self.ipol_gstar(Tp) / 30. * Tp**4 # initial radiation domination rho_RAD = pi^2* gstar(T[0])/30*T^4
+        y0      = np.array([0., 0.])
+        nphi    = (2.*zeta(3)/np.pi**2) * Tp**3
+        params  = np.array([Tp, epstt, epsmm, epsee, np.real(rRadi), 1.])
+        af = 100000.
+        t1 = np.linspace(1., af, num=10000, endpoint=True)
 
         # solve equation
-        ys          = odeint(self.RHS, y0, t1, args = tuple(params))
+        ys      = odeint(self.RHS, y0, t1, args = tuple(params))
         # functions for converting to etaB using the solution to find temp
-        T           = ys[:, 1]
-        gstarSrec   = self.ipol_gstarS(0.3e-9) # d.o.f. at recombination
-        gstarSoff   = self.ipol_gstarS(T[-1])  # d.o.f. at the end of leptogenesis
+        T           = self.Ti/t1
+        gstarSrec = self.ipol_gstarS(0.3e-9) # d.o.f. at recombination
+        gstarSoff = self.ipol_gstarS(T[-1])  # d.o.f. at the end of leptogenesis
         SMspl       = 28./79.
         zeta3       = zeta(3)
         ggamma      = 2.
         coeffNgamma = ggamma*zeta3/np.pi**2
-        Ngamma      = coeffNgamma*(10**t1*T)**3
-        coeffsph    = SMspl * gstarSrec/gstarSoff
+        Ngamma      = coeffNgamma*(t1*T)**3
+        coeffsph    =  SMspl * gstarSrec/gstarSoff
         self.ys = np.empty((len(T), 4))
         self.ys[:,0]=t1
         self.ys[:,1]=T
-        self.ys[:,2]=ys[:,2]
-        self.ys[:,-1] = coeffsph*( ys[:,2])*nphi/Ngamma
+        self.ys[:,2]=ys[:,1]
+        self.ys[:,-1] = coeffsph*( ys[:,1])*nphi/Ngamma
+
+        print(Ngamma)
+
+        plt.plot(np.log(t1), ys[:,0], color='r', label=r'$N_N$')
+        plt.plot(np.log(t1), 1e6*ys[:,1], color='g', label=r'$N_{B-L}\times 10^{6}$')
+        plt.plot(np.log(t1), 1e9*self.ys[:,-1], color='b', label=r'$|\eta_B|\times 10^9$')
+        plt.xlabel(r"$\ln(a)$", fontsize=16)
+        plt.legend(loc='lower right', fontsize=16)
+        plt.ylabel(r"$N_N$, $N_{B-L}$, $|\eta_B|$",  fontsize=16)
+        plt.show()
+
         return self.ys[-1][-1]
